cbd2235cd6128d9511f4f2ef94c40a126c40160a
Heather <heather@thisisnotanorg.org>
2015-03-23 15:08:37 -0700

    Add ability to edit and create lists.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,9 @@
 class ListsController < ApplicationController
+  def new
+    @list = List.new
+    render :new
+  end
+
   def index
     @lists = List.all
     render :index
@@ -7,5 +12,28 @@ class ListsController < ApplicationController
   def show
     @list = List.find(params[:id])
     render :show
+  end
+
+  def create
+    @list = List.new(params[:list])
+    if @list.save
+      redirect_to lists_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @list = List.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @list = List.find(params[:id])
+    if @list.update(params[:list])
+      redirect_to lists_path
+    else
+      render :edit
+    end
   end
 end
