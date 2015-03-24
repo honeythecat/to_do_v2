2f54b066d2217893eb0a90f343542784cda3a9d2
Heather <heather@thisisnotanorg.org>
2015-03-24 16:19:03 -0700

    Add 2 working capybara specs.

---
+++ spec/features/edit_task_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "the edit a task process" do
+  it "edits a task" do
+    list = List.create(:name => 'Home')
+    task = Task.create(:description => 'Wash the dog', :list_id => list.id)
+    visit task_path(task)
+    click_on 'Edit'
+    fill_in 'Description', :with => 'Wash the cat'
+    click_on 'Update Task'
+    expect(page).to have_content 'successfully'
+  end
+
+  it "gives error when no description is entered" do
+    list = List.create(:name => 'Home')
+    task = Task.create(:description => 'Wash the dog', :list_id => list.id)
+    visit task_path(task)
+    click_on 'Update Task'
+    expect(page).to have_content 'errors'
+  end
+
+
+
+end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -14,4 +14,14 @@ describe Task do
     end
   end
 
+  describe '.done' do
+    it 'returns only tasks that are done' do
+      this_list = List.create({ :name => 'homework', :id => 1 })
+      done_task = Task.create({ :description => 'stuff', :done => true, :list_id => 1 })
+      not_done_task = Task.create({ :description => 'fluff', :done => false, :list_id => 1 })
+
+      expect(this_list.tasks.done).to eq([done_task])
+    end
+  end
+
 end
